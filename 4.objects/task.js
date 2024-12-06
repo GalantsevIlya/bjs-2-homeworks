c7ba876d9e8e7d849fbcb62c280d1c28aa85c52b
Galantsev Ilya <Galantsev.1989@gmail.com>
2024-12-06 15:13:06 +1000

    the task has been completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
+  
+let studentOleg = new Student('Oleg', 'male', 25);
+let studentMaria = new Student('Maria', 'female', 23);
+let studentIvan = new Student('Ivan', 'male', 28);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.marks) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    let sum = this.marks.reduce((acc, item) => {
+        return acc + item}, 0)
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
   
+    this.excluded = reason;
 }
